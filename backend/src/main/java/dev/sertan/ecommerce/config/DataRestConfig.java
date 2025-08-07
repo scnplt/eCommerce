8bc78473bb20d138e0f5d7d6f6fba8485027b561
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2025-08-07 10:02:36 +0300

    Enhance DataRestConfig to expose entity IDs

--- backend/src/main/java/dev/sertan/ecommerce/config/DataRestConfig.java
+++ backend/src/main/java/dev/sertan/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,26 @@ package dev.sertan.ecommerce.config;
 
 import dev.sertan.ecommerce.entity.Product;
 import dev.sertan.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -32,5 +44,19 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .withCollectionExposure((_, httpMethods) ->
                         httpMethods.disable(unsupportedActions)
                 );
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
