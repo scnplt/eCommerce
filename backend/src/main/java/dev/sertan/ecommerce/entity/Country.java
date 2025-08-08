85b03d0473028c0b4be360d5e68938d61aca2e74
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2025-08-08 16:10:59 +0300

    Add Country and State entities with repositories

---
+++ backend/src/main/java/dev/sertan/ecommerce/entity/Country.java
@@ -0,0 +1,36 @@
+package dev.sertan.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
