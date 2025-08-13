ed083d63288ce11e84e2f99c1eac3cb4343f436c
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2025-08-13 16:01:49 +0300

    Implement user authentication with Auth0

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
     ProductCategoryMenuComponent,
     SearchComponent,
     CartStatusComponent,
+    LoginStatusComponent,
   ],
   templateUrl: './app.html',
 })
